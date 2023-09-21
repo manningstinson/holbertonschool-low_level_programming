fd192f62d02eb84c2d40fd7b3588054714fb2c4d
Manning Stinson <manning@manningstinson.com>
2023-09-21 16:47:20 -0700

    updated 4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,13 @@
-include <stdio.h>
+#include <stdio.h>
+
 /*Include standard input header
  * Main function
  * Using puts to outpput text
  */
 
-main (void) {
-	puts("\"Programming is like building a multilingual puzzle")
+int main (void) {
+	
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 
 }
