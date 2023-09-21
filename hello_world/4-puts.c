1513064a6ebe932d50c41916b77316d07e8a4f4f
Manning Stinson <manning@manningstinson.com>
2023-09-21 16:39:31 -0700

    added 4-puts

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+include <stdio.h>
+/*Include standard input header
+ * Main function
+ * Using puts to outpput text
+ */
+
+main (void) {
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+
+}
