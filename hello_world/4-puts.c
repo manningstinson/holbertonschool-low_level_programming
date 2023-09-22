96290931666dd652224bf12448e5081f20752f74
Manning Stinson <manning@manningstinson.com>
2023-09-21 18:11:06 -0700

    updated 4-puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,13 +1,10 @@
-#include <stdio.h>
-
+#include<stdio.h>
 /**
- * Main function
+ * main - Entry point
  *
- * Returns value (0) successful
+ * Return: Always 0 (Success)
  */
-
 int main(void)
-
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
