539b28de1d4911a442b4def02d3ea8532bcb3215
Manning Stinson <manning@manningstinson.com>
2023-09-24 17:43:10 -0700

    updated task 0

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,2 +1,16 @@
-include "main.h"
+#include "main.h"
+/**
+ * main - Prints _putchar.
+ *
+ * Return: Always 0 (Success)
+ *
+ */
 
+#include <unistd.h>
+
+int main(void)
+{
+	char str[] = "_putchar\n";
+		write(1, str, sizeof(str) - 1);
+	return (0);
+}
