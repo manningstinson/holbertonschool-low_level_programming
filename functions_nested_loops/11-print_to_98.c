8855df29fe208bfea734cd47ea2b4fc82f5e4e6b
Manning Stinson <manning@manningstinson.com>
2023-09-25 11:26:15 -0700

    updating task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -19,12 +19,12 @@ void print_to_98(int n)
 			printf("%d, ", n);
 		}
 	}
-	else
+	else if (n < 98)
 	{
 		for (; n < 98; n++)
 		{
 			printf("%d, ", n);
 		}
-	printf("%d\n", n);
 	}
-}
+	printf("%d\n", n);
+}	
