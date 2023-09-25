561a86bab8da74bb345008e37519945b61ad7221
Manning Stinson <manning@manningstinson.com>
2023-09-25 10:55:06 -0700

    updated task 11

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-#include <stdio.h>
-
 void print_to_98(int n) {
     if (n > 98) {
         for (; n > 98; n--) {
@@ -15,11 +13,5 @@ void print_to_98(int n) {
         }
     }
     printf("%d\n", n);
-}
-
-int main() {
-    int n = 5; /* Change n to any natural number*/
-    print_to_98(n);
-    return 0;
 }
 
