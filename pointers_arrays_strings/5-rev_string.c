c8ef28e6b80521de361369b4785d550b3aa9ead1
Manning Stinson <manning@manningstinson.com>
2023-10-01 06:58:01 -0700

    updated task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -2,21 +2,25 @@
 
 void rev_string(char *s) 
 {
-    int length = 0;
-    int start = 0;
-    int end = length -1;
+       	int length = 0;
+	int start = 0;
+	int end = 0;
+       
+	while (s[length] != '\0') 
+	{
+		length++;
+	}
 
-    while (s[length] != '\0') 
-    {
-        length++;
-    }
+	end = length -1;
 
-    while (start < end) {
-        char temp = s[start];
-        s[start] = s[end];
-        s[end] = temp;
-        start++;
-        end--;
-    }
+	while (start < end) 
+	{
+		
+		char temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+		start++;
+		end--;
+	}
 }
 
