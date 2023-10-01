39b62b1dc9e1ea87ba3bd1306d8df85bbbcade5b
Manning Stinson <manning@manningstinson.com>
2023-09-30 19:54:49 -0700

    updated task 1

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * swap_int - swaping 2 integer using a pointer
+ * @a:integer
+ * @b:integer
+ * Return:void
+ */
+
+void swap_int(int *a, int *b)
+{
+	int x
+
+	x = *a;
+	*a = *b;
+	*b = x;
+}
