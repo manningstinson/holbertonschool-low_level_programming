574546d6949f37113c9c4cecae5a6bacd4429a30
Manning Stinson <manning@manningstinson.com>
2023-10-01 07:07:57 -0700

    updated task 5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,26 +1,33 @@
 #include "main.h"
 
-void rev_string(char *s) 
+/**
+ * rev_string - reverse a string
+ * @s:string
+ * Return:void
+ */
+
+
+void rev_string(char *s)
 {
-       	int length = 0;
+
+	int length = 0;
 	int start = 0;
 	int end = 0;
-       
-	while (s[length] != '\0') 
+
+	while (s[length] != '\0')
 	{
 		length++;
 	}
 
-	end = length -1;
+	end = length - 1;
 
-	while (start < end) 
+	while (start < end)
 	{
-		
 		char temp = s[start];
+
 		s[start] = s[end];
 		s[end] = temp;
 		start++;
 		end--;
 	}
 }
-
