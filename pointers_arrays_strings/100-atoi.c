3a36dcde7dc2e021df9337d87c0753795f4036f1
Manning Stinson <manning@manningstinson.com>
2023-10-02 12:38:01 -0700

    updated task 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -12,27 +12,33 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 
-	while (*s == ' ' || (*s >= '\t' && *s <= '\r'))
+	while (*s)
 	{
+		if (*s >= '0' && *s <= '9')
+		{
+			break;
+		}
+		if (*s == '-')
+		{
+		sign = -1;
+		}
+		if (*s >= '0' || *s <= '9')
+		{
+		break;
+		}
 		s++;
 	}
-
-	if (*s == '+' || *s == '-')
-	{
-		sign = (*s++ == '-') ? -1 : 1;
-	}
-
 	while (*s >= '0' && *s <= '9')
 	{
 		int digit = *s - '0';
 
 		if (result > (2147483647 - digit) / 10)
 		{
-			return (sign == 1) ? 2147483647 : -2147483648;
+			return ((sign == 1) ? 2147483647 : -2147483648);
+			result = result * 10 + digit;
+			s++;
 		}
-		result = result * 10 + digit;
-		s++;
+		return (result * sign);
 	}
-	return (result * sign);
 }
 
