1a7e70783a7a81ea4c3d8d9dbe028e027724ba92
Manning Stinson <manning@manningstinson.com>
2023-10-02 07:07:10 -0700

    updated task 6

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,17 +6,16 @@
  * return:void
  */
 
-void puts2(char *str)
-{
-	int i = 0;
+void puts2(char *str) {
+    int i = 0;
 
-	while (str[i] != '\0')
-
+    while (1) {
+        if (str[i] == '\0') 
 		break;
-	{
-		_putchar(str[i]);
-		i += 2;
-	}
+        _putchar(str[i]);
+        i += 2; 
+    }
 
-	_putchar('\n');
+    _putchar('\n');
 }
+
