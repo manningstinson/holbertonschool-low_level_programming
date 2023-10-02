b4c8e14d7a10f902cefec7f4d4fd2f4b3260d318
Manning Stinson <manning@manningstinson.com>
2023-10-02 08:08:49 -0700

    updated task 7

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts_half - lenght of string
+ * @str:char
+ * Return:int
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int start_index;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+
+	start_index = (length - 1) / 2;
+
+	while (str[start_index] != '\0')
+	{
+		_putchar(str[start_index]);
+		start_index++;
+	}
+	_putchar('\n');
+}
+
