d92ec5437bb295af381d944a423fdd1fc0a1d40e
Manning Stinson <manning@manningstinson.com>
2023-10-05 12:28:42 -0700

    updated code for task 3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -8,4 +8,13 @@
  * Return: The value
  */
 
-int _strcmp(char *s1, char *s2);
+int _strcmp(char *s1, char *s2)
+{
+	int a = 0;
+
+	while (s1[a] == s2[a] && s1[a] != '\0')
+	{
+		a++;
+	}
+	return (s1[a] - s2[a]);
+}
