3cde104b6a08d5dcdcc3e6bddad6ea786f317cd6
Manning Stinson <manning@manningstinson.com>
2023-10-07 09:10:11 -0700

    updated task 4

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -8,4 +8,19 @@
  * Return: void
  */
 
-void reverse_array(int *a, int n);
+void reverse_array(int *a, int n)
+{
+	int start = 0;
+	int end = n - 1;
+
+	while (start < end)
+	{
+		int temp = a[start];
+
+		a[start] = a[end];
+		a[end] = temp;
+
+		start++;
+		end--;
+	}
+}
