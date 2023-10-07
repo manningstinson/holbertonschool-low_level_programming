f25bbaf07d8ff53e4918fd503c54aa5daa15ad5f
Manning Stinson <manning@manningstinson.com>
2023-10-07 13:41:58 -0700

    updated task 7

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -1,11 +1,31 @@
 #include "main.h"
 
 /**
- * _strcmp  - compare  a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * leet - encodes a string into 1337
+ * @s: string to encode
+ *
+ * Return: address of s
  */
 
-char *leet(char *);
+char *leet(char *s)
+{
+	int i, j;
+	char a[] = "aAeEoOtTlL";
+	char b[] = "4433007711";
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (s[i] == a[j])
+			{
+				_putchar(b[j]);
+				break;
+			}
+		}
+		if (!a[j])
+			_putchar(s[i]);
+	}
+	return (s);
+}
+
