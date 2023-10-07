a15e916a6fe819ce199aa16397ed2b14696f0921
Manning Stinson <manning@manningstinson.com>
2023-10-07 13:23:49 -0700

    updated task 6 - betty styles

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -8,27 +8,41 @@
  * Return: address of modified string
  */
 
-char *cap_string(char *s) {
-    int a = 0;
-    int capitalize_next = 1;
+char *cap_string(char *s)
+{
+	int a = 0;
+	int capitalize_next = 1;
 
-    for (; s[a] != '\0'; a++) {
-        if (s[a] == ' ' || s[a] == '\t' || s[a] == '\n' ||
-            s[a] == ',' || s[a] == ';' || s[a] == '.' ||
-            s[a] == '!' || s[a] == '?' || s[a] == '"' ||
-            s[a] == '(' || s[a] == ')' || s[a] == '{' ||
-            s[a] == '}')
-        {
-            capitalize_next = 1;
-        } else {
-            if (capitalize_next) {
-                if (s[a] >= 'a' && s[a] <= 'z') {
-                    s[a] -= 'a' - 'A';
-                }
-                capitalize_next = 0;
-            }
-        }
-    }
-
-    return (s);
+	for (; s[a] != '\0'; a++)
+	{
+		if (
+			s[a] == ' ' ||
+			s[a] == '\t' ||
+			s[a] == '\n' ||
+			s[a] == ',' ||
+			s[a] == ';' ||
+			s[a] == '.' ||
+			s[a] == '!' ||
+			s[a] == '?' ||
+			s[a] == '"' ||
+			s[a] == '(' ||
+			s[a] == ')' ||
+			s[a] == '{' ||
+			s[a] == '}')
+		{
+			capitalize_next = 1;
+		}
+		else
+		{
+			if (capitalize_next)
+			{
+				if (s[a] >= 'a' && s[a] <= 'z')
+				{
+					s[a] -= 'a' - 'A';
+				}
+				capitalize_next = 0;
+			}
+		}
+	}
+	return (s);
 }
