7f89a4d845feacc24022636db2139c1b3b9902b6
Manning Stinson <manning@manningstinson.com>
2023-10-07 10:52:55 -0700

    updated task 6

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,11 +1,34 @@
 #include "main.h"
+#include <ctype.h>
+#include <string.h>
 
 /**
- * _strcmp  - compare  a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * cap_string - captilize all words in a string
+ * @s:string
+ * Return: address of modified string
  */
 
-char *cap_string(char *);
+char *cap_string(char *s) {
+    int a = 0;
+    int capitalize_next = 1;
+
+    for (; s[a] != '\0'; a++) {
+        if (s[a] == ' ' || s[a] == '\t' || s[a] == '\n' || s[a] == ',' || s[a] == ';' || s[a] == '.' || s[a] == '!' || s[a] == '?' || s[a] == '"' || s[a] == '(' || s[a] == ')' || s[a] == '{' || s[a] == '}') {
+            capitalize_next = 1;
+        } else {
+            if (capitalize_next) {
+                if (s[a] >= 'a' && s[a] <= 'z') {
+                    _putchar(s[a] - 32);
+                } else 
+		{
+                    _putchar(s[a]);
+		}
+                capitalize_next = 0;
+            } else {
+                _putchar(s[a]);
+            }
+        }
+    }
+
+    return s;
+}
