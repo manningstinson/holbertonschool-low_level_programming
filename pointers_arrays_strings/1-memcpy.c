e32ae2985e22deffb36415cf258dd559d3af4e5b
Manning Stinson <manning@manningstinson.com>
2023-10-07 23:39:15 -0700

    updated task 1 - project 2

--- pointers_arrays_strings/1-memcpy.c
+++ pointers_arrays_strings/1-memcpy.c
@@ -1,5 +1,26 @@
 #include "main.h"
 
 
+/**
+ * _memcpy - copies memory area
+ * @dest:char
+ * @src:char
+ * @n:int
+ * Return:char pointer
+ */
 
-char *_memcpy(char *dest, char *src, unsigned int n);
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *dest_ptr = dest;
+	char *src_ptr = src;
+
+	while (n > 0)
+	{
+		*dest_ptr = *src_ptr;
+		dest_ptr++;
+		src_ptr++;
+		n--;
+	}
+	return (dest);
+}
