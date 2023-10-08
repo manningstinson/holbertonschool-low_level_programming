9fb66106289c9f9a11755e90aae7a482d9291abb
Manning Stinson <manning@manningstinson.com>
2023-10-08 01:57:42 -0700

    updated task 6 - project 2

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -1,11 +1,23 @@
 #include "main.h"
 
 /**
- * _strncpy - copie a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * print_chessboard - print a chessboard
+ * @a:array
+ * Return:void
  */
 
-void print_chessboard(char (*a)[8]);
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			char current_char = a[i][j];
+
+			_putchar(current_char);
+		}
+		_putchar('\n');
+	}
+}
