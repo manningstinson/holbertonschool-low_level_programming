a4d33c93f124c27479a050928761507c3f5b0412
Manning Stinson <manning@manningstinson.com>
2023-10-08 00:03:42 -0700

    updatd task 2- project 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,11 +1,21 @@
 #include "main.h"
 
 /**
- * _strncpy - copie a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * _strchr - locate a character in a string
+ * @s:string
+ * @c:char
+ * Return:pointer to char*
  */
 
-char *_strchr(char *s, char c);
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return ((char *)0);
+}
