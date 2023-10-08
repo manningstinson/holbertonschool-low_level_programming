5f7e1a8dbe38e522b3cef519342bc8a09ffbf4b2
Manning Stinson <manning@manningstinson.com>
2023-10-08 00:32:57 -0700

    updated task 3 -proejct 2

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -1,11 +1,34 @@
 #include "main.h"
 
 /**
- * _strncpy - copie a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * _strspn - get the lenght of prefix substring
+ * @s:string
+ * @accept:string
+ * Return:unsigned int
  */
 
-unsigned int _strspn(char *s, char *accept);
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int count = 0;
+	int found = 0;
+	int i = 0;
+
+	while (*s)
+	{
+		for (; accept[i]; i++)
+		{
+			if (*s == accept[i])
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (!found)
+		{
+			break;
+		}
+		count++;
+		s++;
+	}
+	return (count);
+}
