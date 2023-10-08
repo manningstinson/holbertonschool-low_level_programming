44baebce223bcf3cc32f7229296a764a3337e6c6
Manning Stinson <manning@manningstinson.com>
2023-10-08 05:37:50 -0700

    updated task 7- project 2

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,11 +1,23 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _strncpy - copie a string
- * @dest:char
- * @src:char
- * @n:int
- * Return:char
+ * print_diagsums - print sum of 2 digona
+ * @a:int
+ * @size:int size
+ * Return:void
  */
 
-void print_diagsums(int *a, int size);
+void print_diagsums(int *a, int size)
+{
+	int sum_primary = 0;
+	int sum_secondary = 0;
+	int i = 0;
+
+	for (; i < size; i++)
+	{
+		sum_primary += a[i * size + i];
+		sum_secondary += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", sum_primary, sum_secondary);
+}
