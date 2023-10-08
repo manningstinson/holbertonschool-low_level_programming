509951c3661e71b8eb4f695926c9203dd7361c2e
Manning Stinson <manning@manningstinson.com>
2023-10-07 23:20:09 -0700

    updatedtask 0 - project 2

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,10 +2,23 @@
 
 /**
  * _memset  - fills memory with constant byte
- * @dest:char
- * @src:char
- * @n:int
+ * @s:char
+ * @b:char
+ * @n:unsigned int
  * Return:char
  */
 
-char *_memset(char *s, char b, unsigned int n);
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	char *ptr = s;
+
+	while (n > 0)
+	{
+		*ptr = b;
+		ptr++;
+		n--;
+	}
+return (s);
+}
+
