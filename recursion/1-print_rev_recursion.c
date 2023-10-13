4bbc40e7e00761da945ed44ffdcbd572d2f58888
Manning Stinson <manning@manningstinson.com>
2023-10-13 11:31:50 -0700

    updated task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - length of string
+ * @s:string
+ * Return:int
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
