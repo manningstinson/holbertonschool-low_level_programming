db8eba1736cacea8f987b2984ebf10b3095488cb
Manning Stinson <manning@manningstinson.com>
2023-10-15 08:44:22 -0700

    updated function definitions, and header file

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -10,27 +10,11 @@
 
 int is_prime_number(int n)
 {
-	int i = 5;
-
 	if (n <= 1)
-	{
 		return (0);
-	}
-	if (n <= 3)
-	{
-		return (1);
-	}
-	if (n % 2 == 0 || n % 3 == 0)
-	{
+	if (divisor == 1)
+		return (1); 
+	if (n % divisor  == 0)
 		return (0);
-	}
-	for (; i * i <= n; i += 6)
-	{
-		if (n % i == 0 || n % (i + 2)  == 0)
-		{
-			return (0);
-		}
-	}
-
-	return (1);
+	return (is_prime_recursive(n, divisor - 1));
 }
