03c6706ce050efdbaa837fe51b53c60bc85b3214
Manning Stinson <manning@manningstinson.com>
2023-10-15 22:40:58 -0700

    updated task 0, project 2

--- static_libraries/_putchar.c
+++ static_libraries/_putchar.c
@@ -1,16 +1,4 @@
 #include "main.h"
 #include <unistd.h>
 
-/**
- * main - Prints _putchar.
- *
- * Return: Always 0 (Success)
- *
- */
-
-int main(void)
-{
-	char str[] = "_putchar\n";
-		write(1, str, sizeof(str) - 1);
-	return (0);
-}
+int _putchar(char c);
