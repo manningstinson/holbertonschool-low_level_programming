c7e8142f254b9399c4d6426b8738585d4db10823
Manning Stinson <manning@manningstinson.com>
2023-10-20 13:48:19 -0700

    project 1, task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stddef.h>
 #include <string.h>
+#include <stdlib.h>
 
 /**
  * str_concat - Concatenates two strings.
@@ -16,25 +17,26 @@ char *str_concat(char *s1, char *s2)
 {
 	char *concatenated = NULL;
 	unsigned int len1 = 0, len2 = 0;
-	unsigned int a = 0;
-	unsigned int b = 0;
+	unsigned int a, b;
 
 	if (s1 == NULL)
 		s1 = "";
+
 	if (s2 == NULL)
 		s2 = "";
-
 	len1 = strlen(s1);
 	len2 = strlen(s2);
 
+	concatenated = (char *)malloc(len1 + len2 + 1);
+
 	if (concatenated == NULL)
-	return (NULL);
+		return (NULL);
 
-	for (; a < len1; a++)
-	concatenated[a] = s1[a];
+	for (a = 0; s1[a] != '\0'; a++)
+		concatenated[a] = s1[a];
 
-	for (; b  < len2; b++)
-	concatenated[a + b] = s2[b];
+	for (b = 0; s2[b] != '\0'; b++)
+		concatenated[a + b] = s2[b];
 
 	concatenated[a + b] = '\0';
 
