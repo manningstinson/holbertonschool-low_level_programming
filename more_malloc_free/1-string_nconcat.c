f926dbdb24f065d9f46ce3754e7a39999dfd1f8d
Manning Stinson <manning@manningstinson.com>
2023-10-21 09:22:12 -0700

    updated project 2, task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,29 +2,41 @@
 #include "main.h"
 
 /**
- * _calloc - allocates memory for an array and initializes to zero
- * @nmemb: number of elements
- * @size: size of each element
+ * string_noncat - concatenate two strings - n bytes
+ * @s1: first string
+ * @s2: second string
+ * @n: number of bytes of s2 - concatenate
  *
- * Return: pointer to allocated memory
+ * Return: pointer to allocated space with concatenated string
  */
 
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int a;
-	void *ptr;
+	unsigned int s1_len = 0, s2_len = 0;
+	char *result;
+	unsigned int a, b;
 
-	if (nmemb == 0 || size == 0)
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	while (s1[s1_len] != '\0')
+		s1_len++;
+	while (s2[s2_len] != '\0')
+		s2_len++;
+
+	if (n >= s2_len)
+		n = s2_len;
+
+	result = (char *)malloc(s1_len + n + 1);
+	if (result == NULL)
 		return (NULL);
 
-	ptr = malloc(nmemb * size);
-
-	if (ptr == NULL)
-		return (NULL);
-
-	for (a = 0; a < (nmemb * size); a++)
-	{
-		*((char *)ptr + a) = 0;
-	}
-	return (ptr);
+	for (a = 0; a < s1_len; a++)
+		result[a] = s1[a];
+	for (b = 0; b < n; b++, a++)
+		result[a] = s2[b];
+	result[a] = '\0';
+return (result);
 }
