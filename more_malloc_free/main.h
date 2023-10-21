a6797dc7df18e36a4d7db871626b5799681e703e
Manning Stinson <manning@manningstinson.com>
2023-10-21 07:55:00 -0700

    fixed main.h file

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,13 +2,9 @@
 #define MAIN
 /*Contents of the header file*/
 
-
-typedef void *(*malloc_checked_t)(unsigned int);
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 
 #endif
-
-f
