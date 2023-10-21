9b994124e3d17d1d5642e5889e8a102f273350d1
Manning Stinson <manning@manningstinson.com>
2023-10-21 10:41:08 -0700

    updated project 3, task 2

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("This program was compiled from: %s\n", __FILE__);
+	return (0);
+}
