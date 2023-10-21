5af9614b5d5c8a7567044832e12709c624663862
Manning Stinson <manning@manningstinson.com>
2023-10-21 11:35:25 -0700

    project 3 -task 4

---
+++ preprocessor/4-sum.h
@@ -0,0 +1,12 @@
+#ifndef FUNCTION_LIKE_MACRO_SUM
+#define FUNCTION_LIKE_MACRO_SUM
+
+/*
+ * Header file that defines a function like macro - sum
+ * SUM - computes the sum of x, and y
+ */
+
+#define SUM(x, y) ((x) + (y))
+
+
+#endif
