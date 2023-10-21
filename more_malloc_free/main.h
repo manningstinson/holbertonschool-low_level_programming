f2ae0887d027042fab9e9678eead8d85d76e52e0
Manning Stinson <manning@manningstinson.com>
2023-10-21 07:35:19 -0700

    updated project 2, task 0

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -2,6 +2,8 @@
 #define MAIN
 /*Contents of the header file*/
 
+
+typedef void *(*malloc_checked_t)(unsigned int)
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
