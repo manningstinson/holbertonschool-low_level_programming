e728d8bf53d30849613373881a2d9e2451cb37c5
Manning Stinson <manning@manningstinson.com>
2023-10-27 08:40:03 -0700

    updated project 1, task 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,1 +1,17 @@
+#include "dog.h"
+#include <stdlib.h>
 
+/**
+ * init_dog - Initializes struct dog
+ * @d: Pointer to the struct dog
+ * @name: Name of the dog
+ * @age: Age of the dog
+ * @owner: Owner of the dog
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
