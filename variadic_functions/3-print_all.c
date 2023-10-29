c814d611d60033f37a7fc5027bbf5b2e4441d5f8
Manning Stinson <manning@manningstinson.com>
2023-10-29 16:00:32 -0700

    updated project 3, task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -2,52 +2,94 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * p_char - Print a character
+ * @list: Argument list
+ */
 void p_char(va_list list) {
-    char c;
-    c = va_arg(list, int);
+    char c = va_arg(list, int);
     printf("%c", c);
 }
 
+/**
+ * p_string - Print a string
+ * @list: Argument list
+ */
 void p_string(va_list list) {
-    char *str;
-    str = va_arg(list, char*);
+    char *str = va_arg(list, char*);
     printf("%s", (str != NULL) ? str : "(nil)");
 }
 
+/**
+ * p_integer - Print an integer
+ * @list: Argument list
+ */
 void p_integer(va_list list) {
-    int i;
-    i = va_arg(list, int);
+    int i = va_arg(list, int);
     printf("%d", i);
 }
 
+/**
+ * p_float - Print a float
+ * @list: Argument list
+ */
 void p_float(va_list list) {
-    double f;
-    f = va_arg(list, double);
+    double f = va_arg(list, double);
     printf("%f", f);
 }
 
-
-void print_all(const char * const format, ...)
-{
-    int printed = 0;
-    char *s = "";
-    void (*print_functions[])(va_list) = {p_float, p_char, p_string, p_integer};
-    va_list valist;
-    char *format_copy = (char *)format;
-
+/**
+ * print_all - Print various data types based on format string
+ * @format: Format string
+ */
+void print_all(const char * const format, ...) {
+    
+	va_list valist;
+	int printed = 0;
+	char *s = "";
+	const char *format_ptr = format;
+    
     va_start(valist, format);
 
-    while (*format_copy && printed < 4) {
-        char c = *format_copy;
-        if (c >= 'c' && c <= 'f') {
-            printf("%s", s);
-            print_functions[c - 'c'](valist);
-            s = ", ";
-            printed++;
+    while (*format_ptr) {
+        switch (*format_ptr) {
+            case 'c':
+                printf("%s", s);
+                p_char(valist);
+                s = ", ";
+                printed++;
+                break;
+            case 's':
+                printf("%s", s);
+                p_string(valist);
+                s = ", ";
+                printed++;
+                break;
+            case 'i':
+                printf("%s", s);
+                p_integer(valist);
+                s = ", ";
+                printed++;
+                break;
+            case 'f':
+                printf("%s", s);
+                p_float(valist);
+                s = ", ";
+                printed++;
+                break;
+            default:
+                break;
         }
-        format_copy++;
+        format_ptr++;
+        if (printed >= 4) {
+            break;
+        }
     }
 
     va_end(valist);
     printf("\n");
 }
+
