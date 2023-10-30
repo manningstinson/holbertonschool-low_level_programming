beda43272565f04cb40dd2cd9a2d7db6971c8498
Manning Stinson <manning@manningstinson.com>
2023-10-30 05:38:23 -0700

    updated project 2, task 3, reduce get-op function file to 1 variable

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -11,16 +11,13 @@ int (*get_op_func(char *s))(int, int)
         {"%", op_mod},
         {NULL, NULL}
     };
-
+    
     int i = 0;
-    int found = 0;
-
-    while (ops[i].op != NULL && !found)
+    
+    while (ops[i].op != NULL && strcmp(s, ops[i].op) != 0)
     {
-        found = (strcmp(s, ops[i].op) == 0);
         i++;
     }
-
-    return found ? ops[i - 1].f : NULL;
+    
+    return (ops[i].f);
 }
-
