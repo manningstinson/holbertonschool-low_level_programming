824f541a7a2a82a96dae3b370709d4f95e232925
Manning Stinson <manning@manningstinson.com>
2023-10-29 22:29:55 -0700

    updated project 2, task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -8,20 +8,25 @@
  *
  * Return: Index of the first element for which cmp does not return 0, or -1
  */
-int int_index(int *array, int size, int (*cmp)(int)) {
-    if (size <= 0) {
-        return (-1);
-    }
 
-    if (array != NULL && cmp != NULL) {
-        int i, result;
-        for (i = 0; i < size; i++) {
-            result = cmp(array[i]);
-            if (result != 0) {
-                return (i);
-            }
-        }
-    }
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+	{
+		return (-1);
+	}
+	if (array != NULL && cmp != NULL)
+	{
+		int i, result;
 
-    return (-1);
+		for (i = 0; i < size; i++)
+		{
+			result = cmp(array[i]);
+			if (result != 0)
+			{
+				return (i);
+			}
+		}
+	}
+	return (-1);
 }
