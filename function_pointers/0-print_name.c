09736459307e798c0c93a4308cdf90496d93f9f4
Manning Stinson <manning@manningstinson.com>
2023-10-29 22:11:49 -0700

    updated project 2, task o

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,50 +1,15 @@
 #include "function_pointers.h"
-#include <stddef.h>
-
-int _putchar(char c);
 
 /**
- * print_name - Print a name using a callback function
- * @name: Name to be printed
- * @f: Pointer to a function that takes a char* argument
+ * print_name - Function to print a name using a callback function
+ * @name: The name to be printed
+ * @f: The function pointer to format and print the name
  */
 
-void print_name(char *name, void (*f)(char *)) {
-    if (name != NULL && f != NULL) {
-        f(name);
-    }
-}
-
-/**
- * print_name_upper - Print the name in uppercase
- * @name: Name to be printed
- */
-void print_name_upper(char *name) {
-    int i = 0;
-    while (name[i]) {
-        char c = name[i];
-        if (c >= 'a' && c <= 'z') {
-            c -= 32;
-        }
-        _putchar(c);
-        i++;
-    }
-    _putchar('\n');
-}
-
-/**
- * print_name_lower - Print the name in lowercase
- * @name: Name to be printed
- */
-void print_name_lower(char *name) {
-    int i = 0;
-    while (name[i]) {
-        char c = name[i];
-        if (c >= 'A' && c <= 'Z') {
-            c += 32;
-        }
-        _putchar(c);
-        i++;
-    }
-    _putchar('\n');
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+	{
+		f(name);
+	}
 }
