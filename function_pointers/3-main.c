9cf3545ca656e6ee134941ed481601b51b67d3af
Manning Stinson <manning@manningstinson.com>
2023-10-30 05:45:27 -0700

    updated project 2, task 3, fixed failing checks

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -6,9 +6,9 @@ int main(int argc, char *argv[]) {
     int a, b, result;
     int (*fun)(int, int);
 
-    if (argc != 4 || (get_op_func(argv[2]) == NULL)) {
+    if (argc != 4 || (get_op_func(argv[2]) == NULL) || argv[2][1] != '\0') {
         printf("Error\n");
-        return (98 + !argc - !(get_op_func(argv[2]) == NULL));
+        return (98 + !argc - !(get_op_func(argv[2]) == NULL) - (argv[2][1] != '\0'));
     }
 
     a = atoi(argv[1]);
