9e4e0239d3f336396c1b2c71135d8eebf8d73b5f
Manning Stinson <manning@manningstinson.com>
2023-10-29 22:17:16 -0700

    updated project 2, task 1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,19 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Execute a function on each element of an array
+ * @array: The array of integers
+ * @size: The size of the array
+ * @action: Pointer to the function to execute on each element
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array != NULL && action != NULL)
+	{
+		size_t i;
+		for (i = 0; i < size; i++)
+		{
+			action(array[i]);
+		}
+	}
+}
