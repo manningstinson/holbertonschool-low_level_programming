34477887e899466b3c9b7b88ebc582f4148844e6
Manning Stinson <manning@manningstinson.com>
2023-10-30 05:20:09 -0700

    updated project 2, task 3

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,19 +2,32 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
-int main(int argc, char *argv[]) {
-    int a, b, result;
-    int (*fun)(int, int);
+/**
+ * main - performs simple operations
+ * @argc: arg count
+ * @argv: arg value
+ *
+ * Return:int
+ */
+int main(int argc, char *argv[])
+{
+	int a, b, c;
+	int (*fun)(int, int);
 
-    if (argc != 4 || (get_op_func(argv[2]) == NULL)) {
-        printf("Error\n");
-        return (98 + !argc - !(get_op_func(argv[2]) == NULL));
-    }
-
-    a = atoi(argv[1]);
-    b = atoi(argv[3]);
-    fun = get_op_func(argv[2]);
-    result = fun(a, b);
-    printf("%d\n", result);
-    return 0;
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+	fun = get_op_func(argv[2]);
+	if (fun == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	c = fun(a, b);
+	printf("%d\n", c);
+	return (0);
 }
