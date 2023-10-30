6f4574281eb27d529c9a07a0080f0c3ca1b8fee2
Manning Stinson <manning@manningstinson.com>
2023-10-30 05:50:20 -0700

    updated project 2, task 3, fixing last 3 failing checks

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -2,13 +2,20 @@
 #include <stdlib.h>
 #include "3-calc.h"
 
+/**
+ * main - performs simple operations
+ * @argc: argument count
+ * @argv: argument value
+ *
+ * Return: int
+ */
 int main(int argc, char *argv[]) {
     int a, b, result;
     int (*fun)(int, int);
 
-    if (argc != 4 || (get_op_func(argv[2]) == NULL) || argv[2][1] != '\0') {
+    if (argc != 4 || argv[2][1] != '\0' || (get_op_func(argv[2]) == NULL)) {
         printf("Error\n");
-        return (98 + !argc - !(get_op_func(argv[2]) == NULL) - (argv[2][1] != '\0'));
+        return (98 + !argc - (argv[2][1] != '\0') - !(get_op_func(argv[2]) == NULL));
     }
 
     a = atoi(argv[1]);
