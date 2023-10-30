0a8efb203f181b2f5b8eb401bc0ef4a9aab5f6d5
Manning Stinson <manning@manningstinson.com>
2023-10-30 05:33:54 -0700

    updated project 2, task 3, - op function

--- function_pointers/3-get_op_func.c
+++ function_pointers/3-get_op_func.c
@@ -1,31 +1,26 @@
 #include "3-calc.h"
 #include <string.h>
 
-/**
- * get_op_func - Select the correct operation function
- * @s: Operator as a string
- * Return: Pointer to the corresponding function, or NULL on failure
- */
-
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
+    op_t ops[] = {
+        {"+", op_add},
+        {"-", op_sub},
+        {"*", op_mul},
+        {"/", op_div},
+        {"%", op_mod},
+        {NULL, NULL}
+    };
 
-	int i;
-	
-	for (i = 0; ops[i].op != NULL; i++)
-	{
-		if (strcmp(s, ops[i].op) == 0)
-		{
-			return (ops[i].f);
-		}
-	}
-	return (NULL);
+    int i = 0;
+    int found = 0;
+
+    while (ops[i].op != NULL && !found)
+    {
+        found = (strcmp(s, ops[i].op) == 0);
+        i++;
+    }
+
+    return found ? ops[i - 1].f : NULL;
 }
+
