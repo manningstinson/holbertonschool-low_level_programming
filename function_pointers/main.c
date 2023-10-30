23e37d7fa1e1baa46b71a8c17c743f9328888b9e
Manning Stinson <manning@manningstinson.com>
2023-10-29 21:48:36 -0700

    updated project, task 3

--- function_pointers/main.c
+++ function_pointers/main.c
@@ -11,25 +11,24 @@
 
 int main(int argc, char *argv[])
 {
+	int num1 = atoi(argv[1]);
+        int num2 = atoi(argv[3]);
+        char *operator = argv[2];
+        int (*operation)(int, int) = get_op_func(operator);
+	int result = operation(num1, num2);
+
+
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[3]);
-	char *operator = argv[2];
-	int (*operation)(int, int) = get_op_func(operator);
-
 	if (operation == NULL)
 	{
 		printf("Error\n");
 		exit(99);
 	}
-
-	int result = operation(num1, num2);
-
 	printf("%d\n", result);
 	return (0);
 }
