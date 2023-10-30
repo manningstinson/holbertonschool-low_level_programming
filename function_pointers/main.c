a8fc51f74aad66c9ae707415290e2a83e30382c9
Manning Stinson <manning@manningstinson.com>
2023-10-29 21:38:54 -0700

    updated project 2, task 2

---
+++ function_pointers/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "3-calc.h"
+
+/**
+ * main - Entry point of the calculator program
+ * @argc: Number of arguments
+ * @argv: Array of arguments
+ *
+ * Return: 0 on success, other values on failure
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[3]);
+	char *operator = argv[2];
+	int (*operation)(int, int) = get_op_func(operator);
+
+	if (operation == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+
+	int result = operation(num1, num2);
+
+	printf("%d\n", result);
+	return (0);
+}
