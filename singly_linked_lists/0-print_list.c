9445741f1b98d1a617b669f3c1b6e798cb1dd81b
Manning Stinson <manning@manningstinson.com>
2023-11-09 13:22:58 -0800

    updated task 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,27 +8,31 @@
 ssize_t _putchar(char c);
 
 ssize_t read_textfile(const char *filename, size_t letters) {
+    int fd;
+    char *buffer;
+    ssize_t bytes_read;
+    ssize_t bytes_written = 0;
+
     if (filename == NULL)
         return 0;
 
-    int fd = open(filename, O_RDONLY);
+    fd = open(filename, O_RDONLY);
     if (fd == -1)
         return 0;
 
-    char *buffer = malloc(sizeof(char) * letters);
+    buffer = malloc(sizeof(char) * letters);
     if (buffer == NULL) {
         close(fd);
         return 0;
     }
 
-    ssize_t bytes_read = read(fd, buffer, letters);
+    bytes_read = read(fd, buffer, letters);
     if (bytes_read == -1) {
         close(fd);
         free(buffer);
         return 0;
     }
 
-    ssize_t bytes_written = 0;
     if (bytes_read > 0)
         bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 
