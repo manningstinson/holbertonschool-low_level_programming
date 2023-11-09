a89ff0ed1c10349015d40f74ec61e59d51de6179
Manning Stinson <manning@manningstinson.com>
2023-11-09 14:38:00 -0800

    updated task 2

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -9,27 +9,29 @@
  *
  * Return: the address of the new element, or NULL if it failed
  */
-list_t *add_node(list_t **head, const char *str) {
-    
-    list_t *new_node;
-    
-    if (head == NULL || str == NULL)
-        return NULL;
 
-    new_node = malloc(sizeof(list_t));
-    if (new_node == NULL)
-        return NULL;
 
-    new_node->str = strdup(str);
-    if (new_node->str == NULL) {
-        free(new_node);
-        return NULL;
-    }
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
+	if (head == NULL || str == NULL)
+		return (NULL);
 
-    return new_node;
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
 }
-
