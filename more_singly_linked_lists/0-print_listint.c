a37f39c64fb5c7a30742ae8c33d95e2f77774a1d
Manning Stinson <manning@manningstinson.com>
2023-11-10 09:51:56 -0800

    updated task 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stddef.h>
 
 /**
  * print_listint - Prints all the elements of a listint_t list.
@@ -6,6 +8,8 @@
  *
  * Return: The number of nodes.
  */
+
+
 size_t print_listint(const listint_t *h)
 {
     size_t count = 0;
