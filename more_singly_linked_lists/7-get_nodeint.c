c537c9d9d5540977f924e9759204d74ae3cebe3d
Manning Stinson <manning@manningstinson.com>
2023-11-11 07:22:23 -0800

    updated task 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -9,17 +9,16 @@
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
-    unsigned int count = 0;
-    listint_t *current = head;
+	unsigned int count = 0;
+	listint_t *current = head;
 
-    while (current != NULL)
-    {
-        if (count == index)
-            return current;
+	while (current != NULL)
+	{
+		if (count == index)
 
-        count++;
-        current = current->next;
-    }
-
-    return (NULL);
+			return (current);
+		count++;
+		current = current->next;
+	}
+	return (NULL);
 }
