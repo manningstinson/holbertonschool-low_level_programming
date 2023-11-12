565f484bc818b863e1d1978fcd07b3803223ff0c
Manning Stinson <manning@manningstinson.com>
2023-11-12 14:34:41 -0800

    updated betty styles

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -10,28 +10,27 @@
  *
  * Return: 1 on success, -1 on failure
  */
+
 int create_file(const char *filename, char *text_content)
 {
-    int fd, w;
-    size_t len = 0;
+	int fd, w;
+	size_t len = 0;
 
-    if (!filename)
-        return (-1);
+	if (!filename)
+		return (-1);
 
-    fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-    if (fd == -1)
-        return (-1);
+	fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (fd == -1)
+		return (-1);
 
-    if (text_content)
-    {
-        while (text_content[len])
-            len++;
-
-        w = write(fd, text_content, len);
-        if (w == -1)
-            return (-1);
-    }
-
-    close(fd);
-    return (1);
+	if (text_content)
+	{
+		while (text_content[len])
+			len++;
+		w = write(fd, text_content, len);
+		if (w == -1)
+			return (-1);
+	}
+	close(fd);
+	return (1);
 }
