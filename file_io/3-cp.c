d1fa243f0137fc89cbd755623a65a2781fa830cc
Manning Stinson <manning@manningstinson.com>
2023-11-12 13:44:03 -0800

    updated task 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define BUFFER_SIZE 1024
+
+/**
+ * main - Entry point
+ * @argc: The number of command line arguments
+ * @argv: The command line arguments
+ *
+ * Return: 0 on success, or one of 97, 98, 99, 100 on error
+ */
+int main(int argc, char *argv[])
+{
+    int fd_from, fd_to, read_bytes, write_bytes;
+    char buffer[BUFFER_SIZE];
+
+    if (argc != 3)
+    {
+        dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+        exit(97);
+    }
+
+    fd_from = open(argv[1], O_RDONLY);
+    if (fd_from == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        exit(98);
+    }
+
+    fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+    if (fd_to == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+        close(fd_from);
+        exit(99);
+    }
+
+    while ((read_bytes = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+    {
+        write_bytes = write(fd_to, buffer, read_bytes);
+        if (write_bytes != read_bytes)
+        {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+            close(fd_from);
+            close(fd_to);
+            exit(99);
+        }
+    }
+
+    if (read_bytes == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+        close(fd_from);
+        close(fd_to);
+        exit(98);
+    }
+
+    if (close(fd_from) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        exit(100);
+    }
+
+    if (close(fd_to) == -1)
+    {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        exit(100);
+    }
+
+    return (0);
+}
+
