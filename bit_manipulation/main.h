ab65bea11135d3fde21c87854322877c455ce945
Manning Stinson <manning@manningstinson.com>
2023-11-12 07:24:34 -0800

    updated task 2

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <stdio.h>
 
+
+int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
