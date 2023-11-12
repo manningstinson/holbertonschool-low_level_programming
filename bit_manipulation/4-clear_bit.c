1f311e396a74f8a21e5ef58b54c2a0957d61b0b6
Manning Stinson <manning@manningstinson.com>
2023-11-12 07:39:31 -0800

    updated task 4

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -4,6 +4,7 @@
  * @index: The index of the bit to clear.
  * Return: 1 if it worked, or -1 if an error occurred.
  */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index >= sizeof(unsigned long int) * 8)
