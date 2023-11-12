19fb7ab261d311da1c7812bef7a795c9b242b8a7
Manning Stinson <manning@manningstinson.com>
2023-11-12 13:30:32 -0800

    updated task 1

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,6 +5,16 @@
 #include <stddef.h>
 #include <stdlib.h>
 
+/**
+ * read_textfile - Reads a text file and prints it to the POSIX SO.
+ * @filename: The name of the file to read.
+ * @letters: The number of letters to read and print.
+ *
+ * Return: The actual number of letters read and printed.
+ *         0 if the file cannot be opened, read, or if write fails.
+ */
+
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
     int fd;
--- file_io/main.h
+++ file_io/main.h
@@ -4,6 +4,8 @@
 #include <stddef.h>
 #include <unistd.h>
 
+
+int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 
