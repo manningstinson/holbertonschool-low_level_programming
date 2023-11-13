c711fafb0b9ad4aac44fc8277582dd67d6ecffcd
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-11-13 11:19:58 -0600

    Delete structures_typedef directory

--- structures_typedef/README.md
+++
@@ -1,2 +0,0 @@
-![28](https://github.com/manningstinson/holbertonschool-low_level_programming/assets/104523090/c6b3fb0c-0e2b-4c10-aa58-46bdae3b798e)
-# Structures & Typedef
--- structures_typedef/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef MAIN
-#define MAIN
-/*Contents of the header file*/
-
-int _putchar(char c);
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-#endif
