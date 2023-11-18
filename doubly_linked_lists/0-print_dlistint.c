18ee600f846a93410dd66974fab128e16568b713
Manning Stinson <manning@manningstinson.com>
2023-11-18 12:36:20 -0800

    task 0, betty styles, doubly linked lists

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -5,17 +5,15 @@
  * @h: Pointer to the head of the list
  * Return: Number of nodes
  */
+
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-
-    while (h != NULL)
-    {
-        printf("%d\n", h->n);
-        h = h->next;
-        count++;
-    }
-
-    return count;
+	size_t count = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+	return count;
 }
-
