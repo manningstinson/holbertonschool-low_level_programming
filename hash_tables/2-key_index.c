70c9cc565020717ad67480f53c9bd0f9d37e33bd
Manning Stinson <manning@manningstinson.com>
2023-11-19 07:44:52 -0800

    updated task 2

--- hash_tables/2-key_index.c
+++ hash_tables/2-key_index.c
@@ -7,11 +7,10 @@
  *
  * Return: Index at which the key/value pair should be stored in the array
  */
+
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    /* Use the hash_djb2 function to calculate the hash value */
-       	unsigned long int hash_value = hash_djb2(key);
+	unsigned long int hash_value = hash_djb2(key);
 
-    /* Modulo the hash value by the size to get the index within the array */
-       	return (hash_value % size);
+	return (hash_value % size);
 }
