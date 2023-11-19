6cf02f9724b50467ccfb529980b34a3a174239f3
Manning Stinson <manning@manningstinson.com>
2023-11-19 08:15:23 -0800

    updated task 6

--- hash_tables/6-hash_table_delete.c
+++ hash_tables/6-hash_table_delete.c
@@ -5,28 +5,27 @@
  * hash_table_delete - Deletes a hash table
  * @ht: The hash table to be deleted
  */
+
 void hash_table_delete(hash_table_t *ht)
 {
-    unsigned long int i;
-    hash_node_t *current, *temp;
+	unsigned long int i;
+	hash_node_t *current, *temp;
 
-    if (ht == NULL)
-        return;
+	if (ht == NULL)
+		return;
 
-    for (i = 0; i < ht->size; i++)
-    {
-        current = ht->array[i];
-        while (current)
-        {
-            temp = current->next;
-            free(current->key);
-            free(current->value);
-            free(current);
-            current = temp;
-        }
-    }
-
-    free(ht->array);
-    free(ht);
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current)
+		{
+			temp = current->next;
+			free(current->key);
+			free(current->value);
+			free(current);
+			current = temp;
+		}
+	}
+	free(ht->array);
+	free(ht);
 }
-
