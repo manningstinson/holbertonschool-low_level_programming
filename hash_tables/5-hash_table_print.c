0888acf90f4bf0ffa460e17e20d5dd4736e820d8
Manning Stinson <manning@manningstinson.com>
2023-11-19 07:55:08 -0800

    updated task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdio.h>
+
+/**
+ * hash_table_print - Prints a hash table
+ * @ht: The hash table
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+    unsigned long int i;
+    hash_node_t *current;
+    int flag = 0; /* Flag to check if there are any elements to print */
+
+    if (ht == NULL)
+        return;
+
+    printf("{");
+    for (i = 0; i < ht->size; i++)
+    {
+        current = ht->array[i];
+        while (current)
+        {
+            if (flag)
+                printf(", ");
+            printf("'%s': '%s'", current->key, current->value);
+            flag = 1;
+            current = current->next;
+        }
+    }
+    printf("}\n");
+}
+
