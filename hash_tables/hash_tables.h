12d95afdf9847bdd3aaef03633789019ce5c2a1c
Manning Stinson <manning@manningstinson.com>
2023-11-19 07:21:34 -0800

    updated hash_tables

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -1,18 +1,34 @@
-#ifndef HASH_TABLES
-#define HASH_TABLES
+#ifndef HASH_TABLES_H
+#define HASH_TABLES_H
+
+/**
+ * struct hash_node_s - Node of a hash table
+ * @key: The key (string)
+ * @value: The value associated with the key
+ * @next: Pointer to the next node in the collision resolution chain
+ */
 
 typedef struct hash_node_s
 {
-    char *key;
-    char *value;
-    struct hash_node_s *next;
-} hash_node_t;
+	char *key;
+	char *value;
+	struct hash_node_s *next;
+}
+	hash_node_t;
+
+/**
+ * struct hash_table_s - Hash table
+ * @size: The size of the array
+ * @array: Array of pointers to hash nodes
+ */
 
 typedef struct hash_table_s
 {
-    unsigned long int size;
-    hash_node_t **array;
-} hash_table_t;
+	unsigned long int size;
+	hash_node_t **array;
+}
+
+	hash_table_t;
 
 hash_table_t *hash_table_create(unsigned long int size);
 
