2b3c2bbfc5b43bc8bd68b7acb93e05cf91915640
Manning Stinson <104523090+manningstinson@users.noreply.github.com>
2023-12-14 01:25:55 -0600

    Update 0-linear.c

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -11,10 +11,12 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+    size_t i;
+
     if (array == NULL)
         return -1;
 
-    for (size_t i = 0; i < size; i++)
+    for (i = 0; i < size; i++)
     {
         printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 
@@ -24,4 +26,3 @@ int linear_search(int *array, size_t size, int value)
 
     return -1;
 }
-
