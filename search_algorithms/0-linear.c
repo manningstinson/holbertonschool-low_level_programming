53be84637f5426f70a05d623e9982dae6e75f665
Manning Stinson <manning@manningstinson.com>
2023-12-14 00:24:03 -0800

    fixed betty styles

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -2,7 +2,7 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using linear search algorithm
+ * linear_search - Searches for a value in an array using linear search
  * @array: Pointer to the first element of the array
  * @size: Number of elements in the array
  * @value: Value to search for
@@ -12,10 +12,10 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	
+
 	if (array == NULL)
 		return (-1);
-	
+
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -11,36 +11,31 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-    size_t left, right, mid, i;
+	size_t left, right, mid, i;
 
-    if (array == NULL)
-        return -1;
+	if (array == NULL)
+		return (-1);
+	left = 0;
+	right = size - 1;
 
-    left = 0;
-    right = size - 1;
+	while (left <= right)
+	{
+		mid = (left + right) / 2;
+		printf("Searching in array: ");
 
-    while (left <= right)
-    {
-        mid = (left + right) / 2;
-
-        printf("Searching in array: ");
-        for (i = left; i <= right; i++)
-        {
-            if (i == right)
-                printf("%d\n", array[i]);
-            else
-                printf("%d, ", array[i]);
-        }
-
-        if (array[mid] == value)
-            return mid;
-
-        if (array[mid] < value)
-            left = mid + 1;
-        else
-            right = mid - 1;
-    }
-
-    return -1;
+		for (i = left; i <= right; i++)
+		{
+			if (i == right)
+				printf("%d\n", array[i]);
+			else
+				printf("%d, ", array[i]);
+		}
+		if (array[mid] == value)
+			return (mid);
+		if (array[mid] < value)
+			left = mid + 1;
+		else
+			right = mid - 1;
+	}
+	return (-1);
 }
-
